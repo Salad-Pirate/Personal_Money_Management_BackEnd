d1abdf0075974c079b69c8fd4e4f33e69200c93d
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2025-09-22 22:18:41 +0700

    Initailize Project

---
+++ pmm_be/src/main/java/salad/example/pmm_be/PmmBeApplication.java
@@ -0,0 +1,13 @@
+package salad.example.pmm_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmmBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmmBeApplication.class, args);
+    }
+
+}
---
+++ pmm_be/src/test/java/salad/example/pmm_be/PmmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package salad.example.pmm_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmmBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
